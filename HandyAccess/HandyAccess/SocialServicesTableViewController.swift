148966c99ab980b14ce909296635686e1180748e
Ana <anama118118@gmail.com>
2017-02-18 17:11:02 -0500

    Parse SocialServices

--- HandyAccess/HandyAccess/SocialServicesTableViewController.swift
+++ HandyAccess/HandyAccess/SocialServicesTableViewController.swift
@@ -69,6 +69,13 @@ class SocialServicesTableViewController: UITableViewController {
             }
         }
     }
+    
+    lazy var filterBarButton: UIBarButtonItem = {
+       let barButton = UIBarButtonItem()
+        barButton.title = "Filter"
+        return barButton
+    }()
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
